2d743929c5889cfbb0b63593f902a61818858dfb
DenanDiep <denan.diep@student.tafesa.edu.au>
2024-03-20 22:19:25 +1030

    Updated Trip Calculator click event message

--- UniversalCalculator/MainMenu.xaml.cs
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -51,9 +52,12 @@ namespace Calculator
 			Environment.Exit(0);
 		}
 
-		private void trip_CalculatorButton_Click(object sender, RoutedEventArgs e)
+		private async void trip_CalculatorButton_Click(object sender, RoutedEventArgs e)
 		{
 			// Trip calculator C# code will be developed later
+			var dialogMsg = new MessageDialog("Trip calculator C# code will be developed later");
+			await dialogMsg.ShowAsync();
+			return;
         }
     }
 }
